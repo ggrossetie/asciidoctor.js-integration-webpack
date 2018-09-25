bd8b7270fa0f42302ded3f18f3eed0453e31ef88
Guillaume Grossetie <ggrossetie@gmail.com>
2018-09-25 17:23:00 +0200

    Remove warnings

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,16 @@
 const config = {
+  mode: 'production',
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: __dirname + '/dist'
   },
   node: {
-    fs: 'empty'
+    fs: 'empty',
+    child_process: 'empty'
+  },
+  performance: {
+    hints: false
   }
 };
 
